5ef236f25da33806e4d92fdd6bc8f975ae758bb4
Lynext <lynext12@gmail.com>
2020-08-16 22:45:21 +0300

    Lynext
    New sigs

--- Test.py
+++ Test.py
@@ -5,7 +5,7 @@ import re
 PROCESS_NAME = b'Brawlhalla.exe'
 
 entity_sig_1 = b"\x00\x90\x64......\xA0.......\x00"
-entity_sig_2 = b"\x00.\x64..............\x00"
+entity_sig_2 = b"\x90\x64\xCB\x63.\x00\x00\x20\x30"
 
 
 def is_base_of_entity(mem, address):
@@ -22,6 +22,7 @@ def is_base_of_entity(mem, address):
 
 def aob_scan(mem, entity_pointers, start, size, pattern=None, offset=0, entity_check=False):
     all_the_bytes = mem.process.read(mem.Address(start), type='bytes', maxlen=size)
+    #print(all_the_bytes)
     matches = re.finditer(pattern, all_the_bytes)
     for match in matches:
         span = match.span()
@@ -49,11 +50,12 @@ def entities_aob_scan(mem):
     for start, size in regions:
         if len(entity_pointers) >= 4:
             break
-        aob_scan(mem, entity_pointers, start, size, pattern=entity_sig_2, offset=1, entity_check=True)
+        aob_scan(mem, entity_pointers, start, size, pattern=entity_sig_2, offset=0, entity_check=False)
 
     print('Found %d entities : %s' % (len(entity_pointers), ', '.join([hex(e) for e in entity_pointers])))
     return entity_pointers
 
-mem = MemWorker(pid=9356)
+mem = MemWorker(pid=16492)
 modules = mem.process.list_modules()
+#print(str(modules))
 entities_aob_scan(mem)
